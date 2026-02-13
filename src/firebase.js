8778cd5ca0e83bcee610d26dfdb2e9efa3a13064
EwanYun <ewan.huggon@hotmail.co.uk>
2026-02-13 13:39:04 +0000

    Updated firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { getStorage } from 'firebase/storage';
 // TODO: Replace with your Firebase config from Firebase Console
 // You'll get this when you create a Firebase project
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAt2fEUd2QxWRsHhpzfGrrEPjmxo0rLdUA",
+  authDomain: "a-n-e-d947a.firebaseapp.com",
+  projectId: "a-n-e-d947a",
+  storageBucket: "a-n-e-d947a.firebasestorage.app",
+  messagingSenderId: "309050151737",
+  appId: "1:309050151737:web:e2e9e91eeb09e1fd6c8ee8"
 };
 
 // Initialize Firebase
