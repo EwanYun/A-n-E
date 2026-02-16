27f0e2d5d83a65718a16d16ebbe3e2751dde0150
EwanYun <ewan.huggon@hotmail.co.uk>
2026-02-16 01:21:21 +0000

    Updated icons

--- src/firebase.js
+++ src/firebase.js
@@ -2,8 +2,6 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
-// TODO: Replace with your Firebase config from Firebase Console
-// You'll get this when you create a Firebase project
 const firebaseConfig = {
   apiKey: "AIzaSyAOqmWHHNxChnrWJhY-xdQGzPDnr9VnqqY",
   authDomain: "a-and-e-830ab.firebaseapp.com",
@@ -13,10 +11,8 @@ const firebaseConfig = {
   appId: "1:657854266376:web:1ef093a261d1f75e02a5bd"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize services
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 export default app;
